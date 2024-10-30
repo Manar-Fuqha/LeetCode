6c776c81749730a7b313fbfdec05ef5505cd66f8
Manar Fuqha <manar0fuqha2002@gmail.com>
2024-10-30 13:40:32 +0300

    Time: 3 ms (47.04%), Space: 45.6 MB (57.18%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int last =0;
+        for(int i=0;i<nums.length;i++){
+            if(i>last)return false;
+            last = Math.max(last , i+nums[i]);
+        }
+        return last>=nums.length-1;
+    }
+}
