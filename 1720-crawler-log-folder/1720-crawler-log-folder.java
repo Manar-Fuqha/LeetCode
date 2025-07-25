e0c6fd9cd4d68a11878d0653d74e5bd8214e5105
Manar Fuqha <manar0fuqha2002@gmail.com>
2025-07-25 08:04:10 +0300

    Time: 1 ms (99.68%), Space: 41.9 MB (91.36%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.java
+++ 1720-crawler-log-folder/1720-crawler-log-folder.java
@@ -1,17 +1,21 @@
 class Solution {
     public int minOperations(String[] logs) {
-        Stack<String> stack = new Stack<>();
+
+        int count=0;
         for(int i=0;i<logs.length;i++){
             if( logs[i].equals("../") ){
-                if(!stack.isEmpty())
-                stack.pop();
+                if(count>0)
+                count--;
                 continue;
             }
             else if(logs[i].equals("./")) continue;
             else{
-                stack.push(logs[i]);
+                count++;
             }
         }
-        return stack.size();
+    return count; 
+
+
+       
     }
 }
